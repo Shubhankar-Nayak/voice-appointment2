3d8d294efac1203db8e47b5e68f79f262fd22762
Shubhankar-Nayak <shubhankarnayak05@gmail.com>
2025-06-19 19:51:32 +1000

    new commit

---
+++ src/redux/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import appointmentReducer from './appointmentSlice';
+
+const store = configureStore({
+  reducer: {
+    appointment: appointmentReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
